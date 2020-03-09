7bd264528150125d4b87f03ed25a065d524676d1
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-03-09 03:47:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,17 @@
+require 'pry'
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  counter = 0
+  while counter < collection.length do
+    hash = collection[counter]
+    if hash[:item] == name
+    my_hash = hash
+    end
+    counter += 1
+  end
+  p my_hash
 end
 
 def consolidate_cart(cart)
@@ -9,18 +19,42 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  my_cart = cart.clone
+  counter = 0
+  while counter < my_cart.length do
+    item = my_cart[counter]
+    if item[:count]
+      item[:count] += 1
+    else
+      item[:count] = 1
+    end
+    counter += 1
+  end
+  my_cart
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  # # REMEMBER: This method **should** update cart
+  cart_counter = 0
+  while cart_counter < cart.length do
+    item_in_cart = cart[cart_counter][:item]
+    if  item_in_cart == coupons[cart_counter][:item]
+
+      item_w_coupon = item_in_cart + " W/COUPON"
+
+    end
+    cart_counter += 1
+  end
+
+  item_w_coupon
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+
 end
 
 def checkout(cart, coupons)
